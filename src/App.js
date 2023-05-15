3f28081f7ac2e4fa7d0d6b2153b028ba123c0688
heowan <101536766+heowan@users.noreply.github.com>
2023-05-15 16:22:28 +0900

    헤더 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import Main from "./pages/main";
+import ProductList from "./pages/productList";
+import Bookmark from "./pages/bookmark";
 import Header from "./component/header";
 
+const GlobalStyle = createGlobalStyle`
+
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  img {
+    display: block;
+  }
+
+  button {
+    background-color: #fff;
+    border: 0;
+    cursor: pointer;
+  }
+
+`;
+
 function App() {
-  return <Header />;
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/products/list" element={<ProductList />} />
+        <Route path="/bookmark" element={<Bookmark />} />
+      </Routes>
+      <footer></footer>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/component/header.js
+++ src/component/header.js
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 import styled from "styled-components";
 import log from "../assets/로고.png";
 import menu from "../assets/햄버거 버튼.png";
+import DropDownMenu from "./dropDownMenu";
+import { useState, useRef, useEffect } from "react";
 
 const HeaderContainer = styled.header`
+  position: relative;
   width: 100%;
   height: 80px;
   display: flex;
@@ -30,6 +35,11 @@ const HeaderLeft = styled.div`
   }
 `;
 
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #000;
+`;
+
 const HeaderRight = styled.div`
   margin-right: 76px;
   cursor: pointer;
@@ -38,17 +48,53 @@ const HeaderRight = styled.div`
     width: 34px;
     height: 24px;
   }
+
+  > div {
+    position: absolute;
+    top: 70px;
+    right: 32px;
+  }
 `;
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    function handleOutside(e) {
+      if (menuRef.current && !menuRef.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    }
+    document.addEventListener("mousedown", handleOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleOutside);
+    };
+  }, []);
+
+  const onClickMenuBtn = () => {
+    setIsOpen(isOpen ? false : true);
+  };
+
   return (
     <HeaderContainer>
-      <HeaderLeft>
-        <img src={log} alt="코드스테이츠 로고" />
-        <h1>COZ Shoppong</h1>
-      </HeaderLeft>
-      <HeaderRight>
-        <img src={menu} alt="메뉴 버튼" />
+      <StyledLink to="/">
+        <HeaderLeft>
+          <img src={log} alt="코드스테이츠 로고" />
+          <h1>COZ Shoppong</h1>
+        </HeaderLeft>
+      </StyledLink>
+
+      <HeaderRight ref={menuRef}>
+        <button onClick={onClickMenuBtn}>
+          <img src={menu} alt="메뉴 버튼" />
+        </button>
+        <div className="dropDownMenu">
+          {isOpen ? (
+            <DropDownMenu onClickMenuBtn={onClickMenuBtn} />
+          ) : undefined}
+        </div>
       </HeaderRight>
     </HeaderContainer>
   );
